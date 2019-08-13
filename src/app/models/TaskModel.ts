f4c94eee89ff22b440d1642f32330501d59a9907
yeshwanth <yeshwanth@cloud-9-solutions.com>
2019-08-13 12:45:51 +0530

    'SetTask'

--- src/app/models/TaskModel.ts
+++ src/app/models/TaskModel.ts
@@ -1,12 +1,16 @@
 export class TaskModel {
+    id : number;
     task: string;
     createdOn: string;
     important: boolean;
     dueDate: string;
-constructor(task: string ,  createdOn: string , important: boolean, dueDate: string) {
+constructor(task: string ,  createdOn: string , important: boolean, dueDate: string, id?: number) {
     this.task = task;
     this.createdOn = createdOn;
     this.important = important;
     this.dueDate = dueDate;
+    this.id = id;
 }
+
+
  }
--- src/app/services/display.service.ts
+++ src/app/services/display.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { SampleData } from '../sampleData/sampleTaskData',
 
 @Injectable({
   providedIn: 'root'
 })
 export class DisplayService {
-
+tasks = [];
+  setTask() {
+    this.tasks = SampleData();
+   }
   constructor() { }
 }
