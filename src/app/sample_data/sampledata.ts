ee4d54ec782a56bf55c94598c0602d2a72a9cac6
Abitha <abitha.mukka@cloud-9-solutions.com>
2019-08-13 11:42:43 +0530

    sample tasks

---
+++ src/app/sample_data/sampledata.ts
@@ -0,0 +1,30 @@
+export function SampleData() {
+const sampleTasksData = [
+    {
+        task : 'meeting',
+        createdOn : '12/08/2019',
+        important : true,
+        dueDate : '12/08/2019'
+    },
+    {
+        task : 'session',
+        createdOn : '13/08/2019',
+        important : true,
+        dueDate : '13/08/2019'
+    },
+    {
+        task : 'KT',
+        createdOn : '12/08/2019',
+        important : true,
+        dueDate : '30/08/2019'
+    },
+    {
+        task : 'sessions',
+        createdOn : '13/08/2019',
+        important : false,
+        dueDate : '30/08/2019'
+    }
+];
+return sampleTasksData;
+}
+
