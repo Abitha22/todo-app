52913063530b6fb1c9339f9801a650e8c8fbe564
Adithya <adithya@cloud-9-solutions.com>
2019-08-20 17:42:05 +0530

    newtask component-TestCases

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '../../../node_modules/@angular/router';
 import { TaskService } from '../services/task.service';
+import { Task } from '../models/task';
 
 @Component({
   selector: 'app-details',
@@ -8,22 +9,20 @@ import { TaskService } from '../services/task.service';
   styleUrls: ['./details.page.scss'],
 })
 export class DetailsPage implements OnInit {
-  getTaskId;
-  taskDetails;
-  Tasks = this.taskservice.getTask();
-  constructor(private router: ActivatedRoute, private taskservice: TaskService) {
+  getTaskId: string;
+  taskDetails: Task;
+  constructor(private router: ActivatedRoute, private taskService: TaskService) {
     this.getTaskId = this.router.snapshot.paramMap.get('id');
     this.getTaskDetails(this.getTaskId);
   }
-
   getTaskDetails(id) {
-    const Tasks = this.taskservice.getTask();
+    const tasks = this.taskService.getTask();
     const idvalue = parseInt(id, 10);
-    const index = Tasks.findIndex(task => task.id === idvalue);
-    this.taskDetails = Tasks[index];
+    const index = tasks.findIndex(task => task.id === idvalue);
+    this.taskDetails = tasks[index];
   }
   updateTask(task) {
-    const updateTask = this.taskservice.updateTask(task);
+    const updateTask = this.taskService.updateTask(task);
   }
   ngOnInit() {
     this.getTaskId = this.router.snapshot.paramMap.get('id');
--- src/app/tasks-list/newtask/newtask.component.spec.ts
+++ src/app/tasks-list/newtask/newtask.component.spec.ts
@@ -1,5 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
 
 import { NewtaskComponent } from './newtask.component';
 
@@ -9,10 +9,10 @@ describe('NewtaskComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NewtaskComponent ],
+      declarations: [NewtaskComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -23,5 +23,36 @@ describe('NewtaskComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should have the method enterTask()', () => {
+    expect(typeof component.enterTask).toBe('function');
+  });
+  it('should have a button to add newTask', () => {
+    fixture.detectChanges();
+    const element: HTMLDivElement = fixture.nativeElement;
+    const content = element.querySelector('.newTask');
+    expect(content.getAttribute('class')).toBe('newTask');
+  });
+  it('should have a button `newTask` which takes data-placement value as `left`', () => {
+    fixture.detectChanges();
+    const element: HTMLDivElement = fixture.nativeElement;
+    const content = element.querySelector('.newTask');
+    expect(content.getAttribute('data-placement')).toBe('left');
+  });
+  describe('Output', () => {
+    it('should call enterTask on click the button', () => {
+      const button = fixture.nativeElement.querySelector('ion-button');
+      spyOn(component, 'enterTask');
+      button.dispatchEvent(new Event('click'));
+      fixture.detectChanges();
+      expect(component.enterTask).toHaveBeenCalled();
+    });
+    it('should emit the event while we call the enterTask()', fakeAsync(() => {
+      const button = fixture.nativeElement.querySelector('ion-button');
+      spyOn(component.outEnterTask, 'emit');
+      button.dispatchEvent(new Event('click'));
+      fixture.detectChanges();
+      expect(component.outEnterTask.emit).toHaveBeenCalled();
+    }));
   });
 });
