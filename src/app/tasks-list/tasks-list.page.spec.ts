8d2b1897a050bbefc09522df9134d28a07cf34af
Adithya <adithya@cloud-9-solutions.com>
2019-08-20 14:16:09 +0530

    Test Cases

--- src/app/tasks-list/tasks-list.page.spec.ts
+++ src/app/tasks-list/tasks-list.page.spec.ts
@@ -1,20 +1,49 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, TestBed , inject} from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject, fakeAsync } from '@angular/core/testing';
 
 import { TasksListPage } from './tasks-list.page';
 import { Tasks } from '../data/tasks';
 import { TaskService } from '../services/task.service';
+import { Task } from '../models/task';
+import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
 
+const dummyTasks: Array<Task> = [
+  {
+    id: 1,
+    title: 'meeting',
+    createdOn: '12/08/2019',
+    important: true,
+    dueDate: '12/08/2019'
+  },
+  {
+    id: 2,
+    title: 'session',
+    createdOn: '13/09/2019',
+    important: true,
+    dueDate: '13/09/2019'
+  }];
+class MockTaskService {
+  getTask() {
+    return dummyTasks;
+  }
+}
 describe('TasksListPage', () => {
   let component: TasksListPage;
   let fixture: ComponentFixture<TasksListPage>;
-
+  let service: TaskService;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TasksListPage ],
+      declarations: [TasksListPage],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [TaskService,
+        {
+          provide: TaskService, useClass: MockTaskService
+
+        }]
     })
-    .compileComponents();
+      .compileComponents();
+    service = TestBed.get(TaskService);
   }));
 
   beforeEach(() => {
@@ -40,7 +69,7 @@ describe('TasksListPage', () => {
     const content = element.querySelector(' ion-content > ion-menu-toggle');
     expect(content.getAttribute('auto-hide')).toBe('false');
   });
-  it('should be able to  take the task values', inject([TaskService], (service: TaskService) => {
+  it('should be able to  take the task values', inject([TaskService], () => {
     component.tasks = Tasks;
     fixture.detectChanges();
     const element: HTMLDivElement = fixture.nativeElement;
@@ -82,4 +111,65 @@ describe('TasksListPage', () => {
     const content = element.querySelector('ion-content > ion-menu-toggle > ion-item > ion-icon');
     expect(content.getAttribute('title')).toBe('view details');
   });
+  it('Should call the TasksService Internally', () => {
+    const getTasks = spyOn(TestBed.get(TaskService), 'getTask');
+    service.getTask();
+    expect(getTasks).toHaveBeenCalled();
+  });
+  it('should send the task while click event is called  ', () => {
+    component.tasks = Tasks;
+    fixture.detectChanges();
+    const element: HTMLDivElement = fixture.nativeElement;
+    const content = element.querySelector('ion-content > ion-menu-toggle > ion-item > ion-icon');
+    expect(content.getAttribute('title')).toBe('view details');
+  });
+  it('should have a icon `trash` to delete task ', () => {
+    component.tasks = Tasks;
+    fixture.detectChanges();
+    const element: HTMLDivElement = fixture.nativeElement;
+    const content = element.querySelector('.delete');
+    expect(content.getAttribute('name')).toBe('trash');
+  });
+  it('should have a icon `trash` which takes slot value as `end`  ', () => {
+    component.tasks = Tasks;
+    fixture.detectChanges();
+    const element: HTMLDivElement = fixture.nativeElement;
+    const content = element.querySelector('.delete');
+    expect(content.getAttribute('slot')).toBe('end');
+  });
+  it('should have a icon `trash` which takes data-placement value as `left`  ', () => {
+    component.tasks = Tasks;
+    fixture.detectChanges();
+    const element: HTMLDivElement = fixture.nativeElement;
+    const content = element.querySelector('.delete');
+    expect(content.getAttribute('data-placement')).toBe('left');
+  });
+  it('should have a icon `trash` which takes data-toggle value as `tooltip`  ', () => {
+    component.tasks = Tasks;
+    fixture.detectChanges();
+    const element: HTMLDivElement = fixture.nativeElement;
+    const content = element.querySelector('.delete');
+    expect(content.getAttribute('data-toggle')).toBe('tooltip');
+  });
+  it('should have a icon `trash` which takes title value as `delete task`  ', () => {
+    component.tasks = Tasks;
+    fixture.detectChanges();
+    const element: HTMLDivElement = fixture.nativeElement;
+    const content = element.querySelector('.delete');
+    expect(content.getAttribute('title')).toBe('delete task');
+  });
+  it('should call updateTask() when event is happend', fakeAsync(() => {
+    spyOn(component, 'updateTask');
+    const btn = fixture.debugElement.query(By.css('ion-icon'));
+    btn.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(component.updateTask).toHaveBeenCalled();
+  }));
+  it('should call deleteTask() when event is happend', fakeAsync(() => {
+    spyOn(component, 'deleteTask');
+    const icon = fixture.debugElement.query(By.css('.delete'));
+    icon.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(component.deleteTask).toHaveBeenCalled();
+  }));
 });
