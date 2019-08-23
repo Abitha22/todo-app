1093ed4b67b3192359e1ea2f16cbd285bb476fb9
yeshwanth <yeshwanth@cloud-9-solutions.com>
2019-08-23 15:51:12 +0530

    Test Cases for Calendar Component

--- src/app/calendar/calendar.component.spec.ts
+++ src/app/calendar/calendar.component.spec.ts
@@ -1,6 +1,6 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
-
+import * as moment from 'moment';
 import { CalendarComponent } from './calendar.component';
 
 describe('CalendarComponent', () => {
@@ -25,19 +25,24 @@ describe('CalendarComponent', () => {
     expect(component).toBeTruthy();
   });
   describe('Output', () => {
-    it('should call date() on click the button', () => {
-      const button = fixture.nativeElement.querySelector('ion-calendar');
-      spyOn(component, 'date');
-      button.dispatchEvent(new Event('click'));
-      fixture.detectChanges();
-      expect(component.date).toHaveBeenCalled();
+   it('should accept ion calendar as selector tag' , () => {
+
+   });
+   it('should check whether calendar is displaying using ion-calendar selector', () => {
+    fixture.detectChanges();
+    const element: HTMLDivElement = fixture.nativeElement;
+    const content = element.querySelector('ion-calendar');
+    expect(content).toBeTruthy();
     });
-    it('should emit the event while we call the date()', fakeAsync(() => {
-      const button = fixture.nativeElement.querySelector('ion-calendar');
+   it('should be true if emitted date value is in  format `DD/MM/YYYY`', () => {
+      component.myDate = moment().format('DD/MM/YYYY');
+      const format = moment(component.myDate, 'DD/MM/YYYY');
+      expect(format.isValid()).toBe(true);
+    });
+   it('should emit selected date when event is triggered', () => {
       spyOn(component.outDate, 'emit');
-      button.dispatchEvent(new Event('click'));
-      fixture.detectChanges();
-      expect(component.outDate.emit).toHaveBeenCalled();
-    }));
+      component.date(moment().format('DD/MM/YYYY'));
+      expect(component.outDate.emit).toHaveBeenCalledWith(moment().format('DD/MM/YYYY'));
+    });
   });
 });
